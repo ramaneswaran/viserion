b0aae0fc6ba3e42ff65dd537f034e4ba6384fc18
ramaneswaran <ramannewdelhi44@gmail.com>
2019-10-20 04:27:56 +0530

    Update webhook with all possible intents

--- routes/webhook.js
+++ routes/webhook.js
@@ -6,10 +6,30 @@ router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
 router.post('/search', (req, res)=>{
-    const name = req.queryResult.parameter.name
-    var response = {
-        'fulfillment_text': 'Dont worry we will find '+name,
+    //Use intent name to give diff response
+
+    
+    const intentName = req.body.intent.displayName;
+    
+    if(intentName == 'searchByName'){
+        const name = req.queryResult.parameter.name;
+        var response = {
+            'fulfillment_text': 'Dont worry we will find '+name,
+        }
+        //Also if result turns out empty return sorry message
     }
+    else if(intentName == 'whatIsStatus'):
+        //Send the medical condition
+    else if(intentName == 'whereIsHE'):
+        //send camp data
+    else if(intentName == 'connect'):
+        //let them know if they can connect
+    else if(intentName == 'searchByAge'):
+        //Refine result or give up
+    else if(intentName == 'scheduleCall'):
+        //Schedule a call and call it a day
+    else if(intentName == 'diffNameSearch'):
+        //End the conversation
     res.json(response)
 })
 
