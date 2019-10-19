8890b14f3977e6df55a64d171ea171ffb501b61a
ramaneswaran <ramannewdelhi44@gmail.com>
2019-10-20 00:57:30 +0530

    Build basic server

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv/config');
+
+//Set up express app
+const app = express();
+
+//Setup body parser for post requests
+app.use(express.json());
+app.use(express.urlencoded({extended :true}));
+
+//Set up a middleware
+app.use(express.static('public'));
+
+
+//CONNECT TO DB
+mongoose.connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+ }).then(()=>{
+     console.log(`Connected to DB`)
+ }).catch(err=>{
+     console.log(`DB error ${err.message}`);
+     process.exit(-1)
+ });
+
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("Listening to port");
+});
