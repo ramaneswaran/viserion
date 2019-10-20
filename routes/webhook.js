88f464ffea726515b9a189c489e77de24c85d867
ramaneswaran <ramannewdelhi44@gmail.com>
2019-10-20 11:47:40 +0530

    Hopefully shit works

--- routes/webhook.js
+++ routes/webhook.js
@@ -12,38 +12,39 @@ router.use(express.urlencoded({extended: true}));
 
 router.post('/search', (req, res)=> {
     //Use intent name to give diff response
-
-    
-    // const intentName = req.body.intent.displayName;
     const name = req.body.queryResult.parameters.name;
-        var response = {
-            'fulfillment_text': 'Initial',
+   
+    //Create fulfillment_text variable
+    var fulfillment_text = 'Some text initially';
+
+     //Create a response object
+     var response = {
+        'fulfillment_text': fulfillment_text,
+    }
+
+    console.log(req.body);
+    Survivor.find({name: {"$regex": name, "$options": "i"}}, (err, docs)=> {
+        console.log('Inside this find function');
+        if(err) fulfillment_text = 'Some error occured';
+        
+        if(!docs) {
+            fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
         }
-       
-        console.log(req.body);
-        Survivor.find({name: {"$regex": name, "$options": "i"}}, (err, docs)=> {
-            console.log('Inside this find function');
-            let fulfilment_text = 'Radnom';
-            if(err) fulfillment_text = 'Some error occured';
-            
-            if(!docs) {
-                fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
+        else{
+            if(docs.length>1){
+                fulfillment_text = 'There were multiple matches, please enter the age';
             }
-            else{
-                if(docs.length>1){
-                    fulfillment_text = 'There were multiple matches, please enter the age';
-                }
-                else if(docs.length == 0){
-                    fulfillment_text = 'There were zero matches';
-                }
-                else {
-                    fulfillment_text = 'We have located '+name;
-                }
+            else if(docs.length == 0){
+                fulfillment_text = 'There were zero matches';
             }
-            response = {
-                'fulfillment_text': fulfilment_text,
+            else {
+                fulfillment_text = 'We have located '+name;
             }
-        });
+        }
+        response = {
+            'fulfillment_text': fulfilment_text,
+        }
+    });
     
     // else if(intentName == 'whatIsStatus'){
     //     //Send the medical condition
