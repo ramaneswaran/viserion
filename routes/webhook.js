804a4938e4f54e8d782473c0635d9ff571dd9f50
ramaneswaran <ramannewdelhi44@gmail.com>
2019-10-20 12:30:31 +0530

    Added yet another fulfillment

--- routes/webhook.js
+++ routes/webhook.js
@@ -27,8 +27,7 @@ router.post('/search', (req, res)=> {
     console.log(req.body);
     if(intentName == 'searchByName'){
         Survivor.find({name: {"$regex": name, "$options": "i"}}, (err, docs)=> {
-            console.log('Inside this find function with name as '+ name);
-            if(err) fulfillment_text = 'Some error occured';
+            if(err) fulfillment_text = 'We encountered an error in finding '+name+'in our database.Please try again';
             
             if(!docs) {
                 fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
@@ -41,7 +40,7 @@ router.post('/search', (req, res)=> {
                     fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
                 }
                 else {
-                    fulfillment_text = 'We have successfully located a'+name+' in one our camps, the age mentioned is '+docs.age;
+                    fulfillment_text = 'We have successfully located a '+name+' in one our camps, the age mentioned is '+docs.age;
                 }
             }
             response = {
@@ -53,19 +52,26 @@ router.post('/search', (req, res)=> {
     }
     else if(intentName == 'whatIsStatus'){
         Survivor.find({name: {"$regex": name, "$options": "i"}}, (err, docs)=> {
-            if(err) fulfillment_text = 'Some error occured';
+            if(err) condition = 'We encountered some error in finding '+name+'in our database.Please try again';
             
             if(!docs) {
-                fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
+                condition = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
             }
             else{
-                const medicalTag = docs.medicalTag;
-                var condition = '';
-                if(medicalTag == 'red') condition = name+' has suffered some injuries and is being attended by doctors';
-                else if(medicalTag == 'blue') condition = name+' has suffered minor injuries and is all right';
-                else condition = name+' has is not injured and is safe'
+                if(docs.length>1){
+                    condition = 'There were multiple matches, please enter the age';
+                }
+                else if(docs.length == 0){
+                    condition = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
+                }
+                else{
+                    const medicalTag = docs.medicalTag;
+                    var condition = '';
+                    if(medicalTag == 'red') condition = name+' has suffered some injuries and is being attended by doctors';
+                    else if(medicalTag == 'blue') condition = name+' has suffered minor injuries and is all right';
+                    else condition = name+' is not injured and is safe'
     
-                
+                }
             }
             response = {
                 'fulfillment_text': condition,
@@ -74,9 +80,36 @@ router.post('/search', (req, res)=> {
             res.json(response);
         });
     }
-    // else if(intentName == 'whereIsHE'){
-    //     //send camp data
-    // }
+    else if(intentName == 'whereIsHE'){
+        Survivor.find({name: {"$regex": name, "$options": "i"}}, (err, docs)=> {
+            if(err) fulfillment_text = 'We encountered some error in finding '+name+'.Please try again';
+            
+            if(!docs) {
+                fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
+            }
+            else{
+                if(docs.length>1){
+                    fulfillment_text = 'There were multiple matches, please enter the age';
+                }
+                else if(docs.length == 0){
+                    fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
+                }
+                else{
+                    const campID = docs.campID;
+                    Camp.find({campID: campID}, (err, docs)=>{
+                        if(err) fulfillment_text = 'We encountered some error in locating the camp.Please try again';
+                        else fulfillment_text = name+ ' is in camp '+campID+' which is located at '+docs.address;
+                    });
+                    
+                }
+            }
+            response = {
+                'fulfillment_text': fulfillment_text,
+            }
+
+            res.json(response);
+        });
+    }
     // else if(intentName == 'connect'){
     //     //let them know if they can connect
     // }
