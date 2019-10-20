0363383acddc5e75cbfbeb79b59f1ff9008959a7
ramaneswaran <ramannewdelhi44@gmail.com>
2019-10-20 12:18:12 +0530

    Add hook for another intent

--- routes/webhook.js
+++ routes/webhook.js
@@ -15,7 +15,7 @@ router.post('/search', (req, res)=> {
     const name = req.body.queryResult.parameters.name.name;
     const intentName = req.body.queryResult.intent.displayName;
 
-    console.log(intentName)
+    
     //Create fulfillment_text variable
     var fulfillment_text = 'Some text initially';
 
@@ -25,34 +25,55 @@ router.post('/search', (req, res)=> {
     }
 
     console.log(req.body);
-    Survivor.find({name: {"$regex": name, "$options": "i"}}, (err, docs)=> {
-        console.log('Inside this find function with name as '+ name);
-        if(err) fulfillment_text = 'Some error occured';
-        
-        if(!docs) {
-            fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
-        }
-        else{
-            if(docs.length>1){
-                fulfillment_text = 'There were multiple matches, please enter the age';
-            }
-            else if(docs.length == 0){
+    if(intentName == 'searchByName'){
+        Survivor.find({name: {"$regex": name, "$options": "i"}}, (err, docs)=> {
+            console.log('Inside this find function with name as '+ name);
+            if(err) fulfillment_text = 'Some error occured';
+            
+            if(!docs) {
                 fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
             }
-            else {
-                fulfillment_text = 'We have successfully located'+name;
+            else{
+                if(docs.length>1){
+                    fulfillment_text = 'There were multiple matches, please enter the age';
+                }
+                else if(docs.length == 0){
+                    fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
+                }
+                else {
+                    fulfillment_text = 'We have successfully located a'+name+' in one our camps, the age mentioned is '+docs.age;
+                }
             }
-        }
-        response = {
-            'fulfillment_text': fulfillment_text,
-        }
+            response = {
+                'fulfillment_text': fulfillment_text,
+            }
 
-        res.json(response);
-    });
+            res.json(response);
+        });
+    }
+    else if(intentName == 'whatIsStatus'){
+        Survivor.find({name: {"$regex": name, "$options": "i"}}, (err, docs)=> {
+            if(err) fulfillment_text = 'Some error occured';
+            
+            if(!docs) {
+                fulfillment_text = 'We couldnt find '+name+' in our database but we are rescuing more people and bringing them to our camps as we speak';
+            }
+            else{
+                const medicalTag = docs.medicalTag;
+                var condition = '';
+                if(medicalTag == 'red') condition = name+' has suffered some injuries and is being attended by doctors';
+                else if(medicalTag == 'blue') condition = name+' has suffered minor injuries and is all right';
+                else condition = name+' has is not injured and is safe'
     
-    // else if(intentName == 'whatIsStatus'){
-    //     //Send the medical condition
-    //     }
+                
+            }
+            response = {
+                'fulfillment_text': condition,
+            }
+
+            res.json(response);
+        });
+    }
     // else if(intentName == 'whereIsHE'){
     //     //send camp data
     // }
