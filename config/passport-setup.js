07ac4204640fc193b3e47c8bcc55b509e6fa3f0c
ramaneswaran <ramannewdelhi44@gmail.com>
2019-10-21 07:26:22 +0530

    Complete Oauth

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -3,6 +3,18 @@ const GoogleStrategy = require('passport-google-oauth20');
 
 //UserModel
 const UserModel = require('../models/UserModel');
+
+passport.serializeUser((user, done) =>{
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) =>{
+    User.findById(id).then((user)=>{
+        done(null, user);
+    })
+    
+});
+
 passport.use(
     new GoogleStrategy({
         //options for strategy 
@@ -10,12 +22,23 @@ passport.use(
         clientID: "822226994276-tvo7s7kdsfn97utr6tnu91mv0dr5epkr.apps.googleusercontent.com" ,
         clientSecret: "qKJnxEl7LtejZjTYCgpu60RP"
     },(accessToken, refreshToken, profile, done)=>{
-        new UserModel({
-            username: profile.displayName,
-            googleId: profile.id
-        }).save().then((savedUser)=>{
-            console.log(savedUser);
-        });
+        //Check if user exists in db
+        UserModel.findOne({googleId: profile.id}).then((currentUser)=>{
+            if(currentUser){
+                //already user in DB
+                console.log('User is :',currentUser);
+                done(null, currentUser);
+            } else{
+                new UserModel({
+                    username: profile.displayName,
+                    googleId: profile.id
+                }).save().then((savedUser)=>{
+                    console.log(savedUser);
+                    done(null, savedUser);
+                });
+            }
+        })
+        
         console.log('passport CB fired')
     })
 )
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const cookieSession = require('cookie-session');
 const passportSetup = require('../config/passport-setup');
+
+
 //Setting up middleware for parsing post request
 router.use(express.json());
 router.use(express.urlencoded({extended: true}));
 
+router.use(cookieSession({
+    maxAge: 24*60*60*1000,
+    keys : ['balerion']
+}));
 
-//Route middle ware
+//Initialise passport
+router.use(passport.initialize());
+router.use(passport.session());
+
+//Middleware
+const authCheck = (req, res, next) =>{
+    if(!req.user){
+        //User not logged in
+        res.redirect('/login');
+
+    }else{
+        next();
+    }
+}
 
 router.get('/', (req, res)=>{
     res.render('index');
@@ -25,15 +45,15 @@ router.get('/google/redirect', passport.authenticate('google'), (req, res)=>{
     res.send('Callback uri');
 })
 
-router.get('/profile', (req, res)=>{
+router.get('/profile', authCheck, (req, res)=>{
     res.render('profile');
 });
 
-router.get('/profile/tyrion', (req, res)=>{
+router.get('/profile/tyrion', authCheck, (req, res)=>{
     res.render('chatbot');
 });
 
-router.get('/profile/connect', (req, res)=>{
+router.get('/profile/connect', authCheck, (req, res)=>{
     res.render('connect')
 });
 
